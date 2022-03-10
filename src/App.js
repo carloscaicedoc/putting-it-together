fa424983fb1a9d5dd42b73ba25bfac15aa3cd3b3
Carlos Caicedo <calicheccmusica@gmail.com>
2022-03-10 14:01:14 -0800

    class components react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import PersonCard from './components/PersonCard.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard lastName={"Simone"} firstName={"Nina"} age={39} hairColor={"Black"}></PersonCard>
+      <PersonCard lastName={"Wolf"} firstName={"Virginia"} age={33} hairColor={"Dark Brown"} location="Berlin"></PersonCard>
+      <PersonCard lastName={"Yourcenar"} firstName={"Marguerite"} age={48} hairColor={"Light Brown"} location="Paris"></PersonCard>
+      <PersonCard lastName={"Thurman"} firstName={"Uma"} age={52} hairColor={"Blonde"} location="Los Angeles"></PersonCard> 
     </div>
   );
 }
-
 export default App;
---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './card.css';
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    birthday = () => {
+        this.setState({ age: this.state.age += 1 })
+    }
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props;
+
+        return (
+        <div className="card">
+            <div className="content">
+                <h2>{lastName}, {firstName}</h2>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button className="birthday-button" onClick={this.birthday}>Happy Birthday</button>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default PersonCard;
